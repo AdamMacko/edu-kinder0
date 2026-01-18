e85bee9837faab919c8cc00a6b81ad45bbe87350
AdamMacko <145450207+AdamMacko@users.noreply.github.com>
2026-01-18 18:15:34 +0100

    Update page.tsx

--- frontend/app/parents/page.tsx
+++ frontend/app/parents/page.tsx
@@ -4,7 +4,7 @@ import Header from "@/app/components/Header";
 import Link from "next/link";
 import { useEffect, useRef, useState } from "react";
 
-const FRONT_API_BASE = process.env.BACKEND_URL ?? "http://localhost:5000";
+const FRONT_API_BASE = process.env.NEXT_PUBLIC_API_BASE ?? "http://localhost:5000";
 const API_BASE = `${FRONT_API_BASE}/api`;
 interface Child {
   id: number;
